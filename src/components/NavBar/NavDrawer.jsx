b3756ad09d4fba4916807de71cb82bf967e939b4
jyeh20 <jmyeh51@gmail.com>
2022-08-16 17:33:20 -0700

    Updated Navbar for mobile

---
+++ src/components/NavBar/NavDrawer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles } from "@material-ui/core/styles";
+
+const NavDrawer = (props) => {
+  const { drawerOpen, toggleDrawer, children } = props;
+
+  const useStyles = makeStyles((theme) => ({
+    "@media (min-width: 768px)": {
+      drawerMobile: {
+        display: "none",
+      },
+    },
+    "@media (max-width: 767px)": {
+      drawerMobile: {
+        height: "100vh",
+      },
+      drawerDesktop: {
+        display: "none",
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <Drawer
+        className={classes.drawerDesktop}
+        anchor={"left"}
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        {children}
+      </Drawer>
+      <Drawer
+        className={classes.drawerMobile}
+        anchor={"bottom"}
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        {children}
+      </Drawer>
+    </>
+  );
+};
+
+export default NavDrawer;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,58 +1,98 @@
-import React from 'react'
+import React from "react";
 
-import colors from '../../scripts/colors'
-import navItems from '../../scripts/navItems'
+import colors from "../../scripts/colors";
+import navItems from "../../scripts/navItems";
 
-import NavbarItem from './NavbarItem'
+import NavbarItem from "./NavbarItem";
 
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from "@material-ui/core/styles";
 
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography'
+import NavDrawer from "./NavDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = (props) => {
-  const { lightMode, drawerOpen, toggleDrawer } = props
+  const { lightMode, drawerOpen, setDrawerOpen, toggleDrawer } = props;
 
   const useStyles = makeStyles((theme) => ({
     layout: {
-      width: '100%',
-      maxWidth: '100%',
-      overflowX: 'hidden',
+      width: "100%",
+      maxWidth: "100%",
+      overflowX: "hidden",
     },
 
     sidebar: {
-      width: '20vw',
-      height: '100%',
+      width: "20vw",
+      height: "100%",
       backgroundColor: lightMode ? colors.sidebarLight : colors.sidebarDark,
-      color: lightMode ? colors.sidebarDark : colors.sidebarLight
+      color: lightMode ? colors.sidebarDark : colors.sidebarLight,
+      paddingTop: "15vh",
     },
 
     sidebarTitle: {
-      fontFamily: 'Cambay',
-      fontSize: '4vh',
-      textAlign: 'center',
-      margin: 'auto',
-      marginTop: '3vh',
-      marginBottom: '3vh',
-    }
-  }))
-  const classes = useStyles()
+      fontFamily: "Cambay",
+      fontSize: "4vh",
+      textAlign: "center",
+      margin: "auto",
+      marginTop: "3vh",
+      marginBottom: "3vh",
+    },
+
+    "@media (max-width: 767px)": {
+      sidebar: {
+        width: "100vw",
+        height: "100vh",
+      },
+      navCloseButtonWrapper: {
+        position: "absolute",
+        left: "50%",
+        transform: "translateX(-50%)",
+        bottom: "6vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    },
+
+    "@media (min-width: 768px)": {
+      navCloseButton: {
+        display: "none",
+      },
+      navCloseButtonWrapper: {
+        display: "none",
+      },
+    },
+  }));
+  const classes = useStyles();
 
   return (
-    <Drawer anchor={"left"} open={drawerOpen} onClose={toggleDrawer(false)}>
+    <NavDrawer drawerOpen={drawerOpen} toggleDrawer={toggleDrawer}>
       <List className={classes.sidebar}>
-        <Typography className={classes.sidebarTitle}>
-          The Eutectics
-        </Typography>
         <Divider light={!lightMode} />
-        {navItems.map((item) =>
-          <NavbarItem lightMode={lightMode} link={item.link} title={item.title} />
-        )}
+        {navItems.map((item, index) => (
+          <NavbarItem
+            key={index}
+            toggleDrawer={toggleDrawer}
+            lightMode={lightMode}
+            link={item.link}
+            title={item.title}
+          />
+        ))}
+        <div className={classes.navCloseButtonWrapper}>
+          <IconButton
+            className={classes.navCloseButton}
+            onClick={() => {
+              setDrawerOpen(false);
+            }}
+          >
+            <CloseIcon sx={{ color: colors.white }} />
+          </IconButton>
+        </div>
       </List>
-    </Drawer>
-  )
-}
+    </NavDrawer>
+  );
+};
 
-export default Navbar
+export default Navbar;
